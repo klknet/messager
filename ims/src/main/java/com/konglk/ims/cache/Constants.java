eb5249154dcd11bde46e947d53d7799417eab461
konglk <konglk@aliyun.com>
2019-06-28 21:56:06 +0800

    多环境

--- ims/src/main/java/com/konglk/ims/cache/Constants.java
+++ ims/src/main/java/com/konglk/ims/cache/Constants.java
@@ -7,7 +7,9 @@ public class Constants {
 
     protected static final String PREFIX = "ims:" ;
     public static final String DEV = "dev";
-    public static final String PRD = "prd";
+    public static final String ALI = "ali";
+    public static final String HOME = "home";
+    public static final String COMPANY = "company";
 
     public static String USER_TICKET = PREFIX+"user:ticket";
     public static String CONV_NUMBER = PREFIX+"conv:num";
